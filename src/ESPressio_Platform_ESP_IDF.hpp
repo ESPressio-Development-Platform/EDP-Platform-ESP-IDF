532f9caba1a03d65090af276737eee93274bca06
Simon Stuart <lakraven@gmail.com>
2026-09-20 13:47:49 +0200

    #2 Export ESP-IDF AtomicWord32 provider

--- src/ESPressio_Platform_ESP_IDF.hpp
+++ src/ESPressio_Platform_ESP_IDF.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "concurrency/AtomicWord32Provider.hpp"
+
 #include "execution/ExecutionContextProvider.hpp"
 #include "memory/MemoryResourceProviders.hpp"
 #include "synchronization/SpinLockProvider.hpp"
