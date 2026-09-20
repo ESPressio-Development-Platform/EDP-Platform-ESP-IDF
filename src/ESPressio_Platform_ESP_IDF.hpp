b1d5c9b5daac497943b8eb1c5dae15b68c75f733
Simon Stuart <lakraven@gmail.com>
2026-09-20 14:13:24 +0200

    #2 Remove rejected AtomicWord32 export

--- src/ESPressio_Platform_ESP_IDF.hpp
+++ src/ESPressio_Platform_ESP_IDF.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "concurrency/AtomicWord32Provider.hpp"
-
 #include "execution/ExecutionContextProvider.hpp"
 #include "memory/MemoryResourceProviders.hpp"
 #include "synchronization/SpinLockProvider.hpp"
